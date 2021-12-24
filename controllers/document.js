61448b58b23cdfdc8c29b5094ecc3bfe5ad69b11
Chirag Lulla <lullachirag239@gmail.coom>
2021-12-24 07:10:32 +0530

    Reconfigure all endpoints according to model

--- controllers/document.js
+++ controllers/document.js
@@ -1,63 +1,83 @@
+const Document = require('../models/document');
+
 // @desc Get All Documents
 // @route GET /api/v1/document
-const getAllDocuments = (req, res) => {
-  const documents = [
-    {
-      id: 1,
-      string: '<h1>Hello I am document 1<h1>',
-    },
-    {
-      id: 2,
-      string: '<h1>Hello I am document 2<h1>',
-    },
-    {
-      id: 3,
-      string: '<h1>Hello I am document 3<h1>',
-    },
-  ];
-  res.status(200).json({
-    status: 'success',
-    data: {
-      documents,
-    },
-  });
+const getAllDocuments = async (req, res) => {
+  try {
+    const documents = await Document.find();
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        documents,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
 // @desc Get Document based on ID
 // @route GET /api/v1/document/:id
-const getDocument = (req, res) => {
-  const { id } = req.params;
-  const doc = {
-    id,
-    string: `Hello I am document ${id}`,
-  };
-  res.status(200).json({
-    status: 'success',
-    data: {
-      doc,
-    },
-  });
+const getDocument = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const doc = await Document.findById(id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        document: doc,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
 // @desc Create new document
 // @route POST /api/v1/document/
-const createDocument = (req, res) => {
-  const newDoc = '<New Document here>';
-  res.status(201).json({
-    status: 'success',
-    data: {
-      newDoc,
-    },
-  });
+const createDocument = async (req, res) => {
+  try {
+    const doc = req.body;
+    const newDoc = await Document.create(doc);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        document: newDoc,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
 // @desc Delete document
 // @route DELETE /api/v1/document/:id
-const deleteDocument = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+const deleteDocument = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Document.findByIdAndDelete(id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failure',
+      message: err,
+    });
+  }
 };
 
 module.exports = {
