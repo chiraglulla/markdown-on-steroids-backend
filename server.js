b537acc974948093ad57f66ea58e7f32af5c7a8b
Chirag Lulla <lullachirag239@gmail.coom>
2021-12-24 07:09:13 +0530

    Connect Database

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then(() => console.log('Database Connected'));
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
