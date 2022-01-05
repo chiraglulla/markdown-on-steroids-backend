183ab93c1865f3379fcd5c6c03fb17f9ba7c7561
Chirag Lulla <lullachirag239@gmail.coom>
2022-01-05 14:24:20 +0530

    Add required headers

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const app = express();
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, DELETE, PATCH, OPTIONS'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Content-Length, X-Requested-With'
+  );
+  next();
+});
 
 app.use('/api/v1/document', documentRouter);
 
