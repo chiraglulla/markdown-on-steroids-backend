d9abbc806cbd10326a098b34b1c5dd8faf2a7778
Chirag Lulla <lullachirag239@gmail.coom>
2022-01-07 20:00:30 +0530

    Handle 404 errors in controllers

--- controllers/document.js
+++ controllers/document.js
@@ -1,102 +1,90 @@
 const Document = require('../models/document');
+const asyncWrapper = require('../utils/asyncWrapper');
+const ErrorHandler = require('../utils/createError');
 
 // @desc Get All Documents
 // @route GET /api/v1/document
-const getAllDocuments = async (req, res) => {
-  try {
-    const documents = await Document.find();
-    res.status(200).json({
-      status: 'success',
-      data: {
-        documents,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'failure',
-      message: err,
-    });
-  }
-};
+const getAllDocuments = asyncWrapper(async (req, res, next) => {
+  const documents = await Document.find();
+  res.status(200).json({
+    status: 'success',
+    data: {
+      documents,
+    },
+  });
+});
 
 // @desc Get Document based on ID
 // @route GET /api/v1/document/:id
-const getDocument = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const doc = await Document.findById(id);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        document: doc,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'failure',
-      message: err,
-    });
+const getDocument = asyncWrapper(async (req, res, next) => {
+  const { id } = req.params;
+  const doc = await Document.findById(id);
+
+  if (!doc) {
+    const err = new ErrorHandler('No such document found', 404);
+    return next(err);
   }
-};
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      document: doc,
+    },
+  });
+});
 
 // @desc Create new document
 // @route POST /api/v1/document/
-const createDocument = async (req, res) => {
-  try {
-    const doc = req.body;
-    const newDoc = await Document.create(doc);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        document: newDoc,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'failure',
-      message: err,
-    });
-  }
-};
+const createDocument = asyncWrapper(async (req, res, next) => {
+  const doc = req.body;
+  const newDoc = await Document.create(doc);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      document: newDoc,
+    },
+  });
+});
 
 // @desc Update Document
 // @route PATCH /api/v1/document/:id
-const updateDocument = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const changes = req.body;
-    await Document.findByIdAndUpdate(id, changes, {
-      runValidators: true,
-    });
-    res.status(204).json({
-      status: 'success',
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'failure',
-      message: err,
-    });
+const updateDocument = asyncWrapper(async (req, res, next) => {
+  const { id } = req.params;
+  const changes = req.body;
+  const doc = await Document.findByIdAndUpdate(id, changes, {
+    runValidators: true,
+    new: true,
+  });
+
+  if (!doc) {
+    const err = new ErrorHandler('No such document found', 404);
+    return next(err);
   }
-};
+
+  res.status(204).json({
+    status: 'success',
+    data: {
+      document: doc,
+    },
+  });
+});
 
 // @desc Delete document
 // @route DELETE /api/v1/document/:id
-const deleteDocument = async (req, res) => {
-  try {
-    const { id } = req.params;
-    await Document.findByIdAndDelete(id);
+const deleteDocument = asyncWrapper(async (req, res, next) => {
+  const { id } = req.params;
+  const doc = await Document.findByIdAndDelete(id);
 
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'failure',
-      message: err,
-    });
+  if (!doc) {
+    const err = new ErrorHandler('No such document found', 404);
+    return next(err);
   }
-};
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 module.exports = {
   getAllDocuments,
