0750a0748f5056560d486fc2d3eba06ace4ed16b
Chirag Lulla <lullachirag239@gmail.coom>
2022-01-08 21:51:01 +0530

    Add event-handlers for uncaught exceptions and unhandled rejections

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION. Shutting down..🔥');
+  process.exit(1);
+});
+
 dotenv.config({ path: './.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -12,4 +18,12 @@ mongoose.connect(DB).then(() => console.log('Database Connected'));
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App is running on port ${port}`));
+const server = app.listen(port, () =>
+  console.log(`App is running on port ${port}`)
+);
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION. Shutting down..🔥');
+  server.close(() => process.exit(1));
+});
