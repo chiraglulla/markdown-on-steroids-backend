c4c0bb11bf2141923fb23c1465b3e23417a0247e
Chirag Lulla <lullachirag239@gmail.coom>
2022-01-08 21:50:21 +0530

    Handle cast errors, duplicate field error, validation error in production

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,28 @@
+const ErrorHandler = require('../utils/createError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path} is ${err.value}`;
+  return new ErrorHandler(message, 400).sendError();
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const [value] = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  const message = `Duplicate field value: ${value}. Please use another value`;
+
+  return new ErrorHandler(message, 400).sendError();
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  console.log(errors);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new ErrorHandler(message, 400).sendError();
+};
+
 const sendDevError = (err, res) => {
-  res.status(err.statusCode || 500).json({
+  res.status(err.statusCode).json({
     error: err,
-    status: err.status || 'error',
+    status: err.status,
     message: err.message,
     stack: err.stack,
   });
@@ -9,7 +30,7 @@ const sendDevError = (err, res) => {
 
 const sendProdError = (err, res) => {
   if (err.isOperational) {
-    res.status(err.statusCode || 500).json({
+    res.status(err.statusCode).json({
       status: err.status || 'error',
       message: err.message,
     });
@@ -24,10 +45,20 @@ const sendProdError = (err, res) => {
 };
 
 const errorHandler = (err, req, res, next) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  console.log(err);
   if (process.env.NODE_ENV === 'development') {
     sendDevError(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendProdError(err, res);
+    let error = { ...err };
+    console.log(err);
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+    sendProdError(error, res);
   }
 };
 
