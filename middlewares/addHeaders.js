fa2e44f4ba738a7ef25cc0b4fc2ad4ecb90ce89f
Chirag Lulla <lullachirag239@gmail.com>
2022-06-08 23:50:46 +0530

    Deployment changes

--- middlewares/addHeaders.js
+++ middlewares/addHeaders.js
@@ -1,5 +1,5 @@
 const addHeaders = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://markdownonsteroids.netlify.app/');
   res.header(
     'Access-Control-Allow-Methods',
     'GET, POST, DELETE, PATCH, OPTIONS'
