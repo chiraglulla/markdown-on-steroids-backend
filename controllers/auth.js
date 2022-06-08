c857f391022803323b00996f038ece7020f9ba0f
Chirag Lulla <lullachirag239@gmail.com>
2022-06-09 00:23:30 +0530

    Deployment changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,6 +24,7 @@ const createAndSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    sameSite: 'None'
   };
 
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
